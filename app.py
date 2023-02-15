a4972b864f8d1cd416038c30785c3d1743279cdd
ELURI ESWARA SATYA PRAVEEN <92803654+praveenesp@users.noreply.github.com>
2023-02-15 21:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
-
+import pandas as pd
 import numpy as np
-from flask import Flask,request,jsonify,render_template,url_for
+from flask import Flask,request,render_template
+from flask_cors import cross_origin
 import pickle
+
 app = Flask(__name__)
 
-sc=pickle.load(open('sc.pkl','rb'))
-model=pickle.load(open('model.pkl','rb'))
+
+model=pickle.load(open('model3.pkl','rb'))
 
 @app.route('/')
+@cross_origin()
 def home():
-    return render_template('index.html')
-@app.route('/predict',methods=['POST'])
+    return render_template('home.html')
+@app.route('/predict',methods=['POST','GET'])
+@cross_origin()
 def predict():
-    l=[]
-    age=int(request.form('Age'))
-    l.append(age)
-    sex=int(request.form('Sex'))
-    l.append(sex)
-    cp=int(request.form('Chest pain Type')) 
-    l.append(cp)
-    chol=int(request.form('Cholestrol'))
-    l.append(chol)
-    fbs=int(request.form('Fasting Blood Sugar'))
-    l.append(fbs)
-    thalach=int(request.form('Max heartrate'))
-    l.append(thalach)
-    exang=int(request.form('Exang'))
-    l.append(exang)
-    oldpeak=int(request.form('oldpeak'))
-    l.append(oldpeak)
-    features=np.array(l)
-    pred=model.predict(sc.transform(features))
-    return render_template('result.html',prediction=pred)
+    if request.method == 'POST':
+        age=int(request.form['Age'])
+        sex=int(request.form['Sex'])
+        cp=int(request.form['Chest pain']) 
+        chol=int(request.form['Cholestrol'])
+        fbs=int(request.form['fbs'])
+        thalach=int(request.form['hattack'])
+        exang=int(request.form['exang'])
+        oldpeak=int(request.form['oldpeak'])
+        ca=int(request.form['ca'])
+        array=np.array([[age,sex,cp,chol,fbs,thalach,exang,oldpeak,ca]])
+
+        features=pd.DataFrame(array, columns = ['age','sex','cp','chol','fbs','thalach','exang','oldpeak','ca'])
+        pred=model.predict(features)
+        if pred[0]:
+            op="Chance of getting Heart Disease"
+        else:
+            op="Cheers!!.. No Diseases"
+        # op="Cheers!!.. No Diseases"
+        return render_template('home.html',output_text = op,Age=age,sex=sex,chest_pain=cp,Cholestrol=chol,fbs=fbs,hattack=thalach,exang=exang,oldpeak=oldpeak,ca=ca)
+        #   return render_template('home.html')
+    return render_template('home.html')
     
 if __name__ == "__main__":
     app.run(debug=True) 
